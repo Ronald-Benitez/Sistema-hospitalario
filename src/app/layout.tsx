7e4347985a1745580a1799be2943f4d7fde18c77
Empanada <58644435+Ronald-Benitez@users.noreply.github.com>
2023-11-07 00:03:50 -0600

    Another try to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from "./providers";
 
+const inter = Inter({ subsets: ['latin'] })
+
 export const metadata: Metadata = {
   title: 'Sistema Hospitalario',
   description: 'Sistema hospitalario para el control de pacientes',
@@ -15,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>
+      <body className={inter.className}>
         <Providers>
           {children}
         </Providers>
